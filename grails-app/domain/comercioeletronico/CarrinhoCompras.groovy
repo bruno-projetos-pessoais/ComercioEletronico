36b44a27c0fc935cba0502c9849d3e72af6127cb
Bruno <bruno.eng.comp@gmail.com>
2016-06-08 13:51:15 -0300

    Adicionar métodos na classe de domínio e testes

--- grails-app/domain/comercioeletronico/CarrinhoCompras.groovy
+++ grails-app/domain/comercioeletronico/CarrinhoCompras.groovy
@@ -2,9 +2,23 @@ package comercioeletronico
 
 class CarrinhoCompras {
 
+    ArrayList<ListaProduto> itens = new ArrayList<ListaProduto>()
+
     static hasMany = [itens: ListaProduto]
 
     static mapWith = "none"
+
+    static transients = ['numeroItens', 'precoTotal']
+
+    Integer getNumeroItens() {
+        return itens.size()
+    }
+
+    BigDecimal getPrecoTotal() {
+        itens.inject(0, { inicial, item ->
+            return inicial + item.precoTotal
+        })
+    }
 
     static constraints = {
     }
--- src/test/groovy/comercioeletronico/CarrinhoComprasSpec.groovy
+++ src/test/groovy/comercioeletronico/CarrinhoComprasSpec.groovy
@@ -1,5 +1,6 @@
 package comercioeletronico
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(CarrinhoCompras)
+@Mock([ListaProduto, CarrinhoCompras])
 class CarrinhoComprasSpec extends Specification {
 
     def setup() {
@@ -15,8 +17,34 @@ class CarrinhoComprasSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Obter número de produtos no carrinho de compras"() {
+        given: "Um carrinho de compras"
+        CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
+
+        and: "Três produtos adicionados no carrinho"
+        carrinhoCompras.addToItens(new ListaProduto())
+        carrinhoCompras.addToItens(new ListaProduto())
+        carrinhoCompras.addToItens(new ListaProduto())
+
+        expect: "Que o número de itens no carrinho seja igual a 3"
+        carrinhoCompras.numeroItens == 3
+
+    }
+
+    void "Calcular preco total do carrinho"() {
+        given: "Um carrinho de compras"
+        CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
+
+        and: "Três produtos adicionados no carrinho"
+        Produto produto1 = new Produto(preco: 10)
+        carrinhoCompras.addToItens(new ListaProduto(produto: produto1, quantidade: 10))
+        Produto produto2 = new Produto(preco: 5)
+        carrinhoCompras.addToItens(new ListaProduto(produto: produto2, quantidade: 1))
+        Produto produto3 = new Produto(preco: 7)
+        carrinhoCompras.addToItens(new ListaProduto(produto: produto3, quantidade: 7))
+
+        expect: "Que o preço total do carrinho seja igual a 154"
+        carrinhoCompras.precoTotal == 154
+
     }
 }

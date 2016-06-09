0a5fc77a7884c7de96a86b98992bad2ac3a44471
Bruno <bruno.eng.comp@gmail.com>
2016-06-09 12:37:52 -0300

    Especificar formatos de resposta, escopo e metodo index

--- grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
+++ grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
@@ -4,6 +4,11 @@ import grails.converters.JSON
 
 class CarrinhoComprasController {
 
+    static responseFormats = ['json', 'xml']
+    static scope = "session"
+    CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
+
     def index() {
+        respond carrinhoCompras
     }
 }

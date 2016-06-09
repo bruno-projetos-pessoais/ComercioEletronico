eae95cce81238ad68999a798e13c3e869cf00783
Bruno <bruno.eng.comp@gmail.com>
2016-06-09 12:38:27 -0300

    Adicionar metodo para adicionar produtos no carrinho e definir allowedMethods

--- grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
+++ grails-app/controllers/comercioeletronico/CarrinhoComprasController.groovy
@@ -7,8 +7,22 @@ class CarrinhoComprasController {
     static responseFormats = ['json', 'xml']
     static scope = "session"
     CarrinhoCompras carrinhoCompras = new CarrinhoCompras()
+    def messageSource
+    static allowedMethods = [index: 'GET', adicionarProduto: 'POST']
 
     def index() {
         respond carrinhoCompras
+    }
+
+    def adicionarProduto(ListaProduto listaProduto) {
+        if (!listaProduto.validate()) {
+            def erros = listaProduto.errors.allErrors.inject([], { listaErros, erro ->
+                (listaErros << messageSource.getMessage(erro, null))
+            })
+            render erros as JSON
+            return
+        }
+        carrinhoCompras.adicionarProduto(listaProduto)
+        render([mensagem: "Produto adicionado com sucesso"] as JSON)
     }
 }

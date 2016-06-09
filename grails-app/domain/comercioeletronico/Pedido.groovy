63220939303d5dbbd5ed800bf9c485f59ec2422a
Bruno <bruno.eng.comp@gmail.com>
2016-06-09 13:18:04 -0300

    Criar classe de pedido

---
+++ grails-app/domain/comercioeletronico/Pedido.groovy
@@ -0,0 +1,18 @@
+package comercioeletronico
+
+class Pedido {
+    List<ListaProduto> itens = new ArrayList<ListaProduto>()
+    static hasMany = [itens: ListaProduto]
+
+    Date dataPedido
+    static transients = ['total']
+
+    BigDecimal getTotal() {
+        itens.inject(0, { inicial, item ->
+            return inicial + item.precoTotal
+        })
+    }
+
+    static constraints = {
+    }
+}
